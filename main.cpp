d147af0dca09e3a44aff2841eec0d6b8fcdcca80
Aleksandr <klimchuk.aa@students.sch2.ru>
2021-09-15 10:07:03 +0300

    Questions from file

--- main.cpp
+++ main.cpp
@@ -3,13 +3,15 @@
 using namespace std;
 
 signed main() {
-    cout << "Your name: ";
-    string name; cin >> name;
-    cout << "Your surname: ";
-    string surname; cin >> surname;
-    cout << "Book you recommend reading: ";
-    string emp; getline(cin, emp);
-    string book; getline(cin, book);
-    ofstream f; f.open("C:\\Users\\Alexander\\Desktop\\survey\\results.txt", ios::app);
-    f << name << ' ' << surname << ' ' << book << "\n";
+    ifstream in; f.open("questions.txt");
+    vector <string> answers;
+    string an;
+    while (getline(in, an)) {
+        answers.push_back(an);
+    }
+    ofstream f; f.open("results.txt", ios::app);
+    for (auto ans : answers) {
+        f << ans << "\n";
+    }
+    f << "\n";
 }
