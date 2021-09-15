680c477a0e5e226e84433693341387f6a917c9f0
Aleksandr <klimchuk.aa@students.sch2.ru>
2021-09-15 10:00:40 +0300

    Function get results

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+void get_results() {
+    ifstream in; in.open("results.txt");
+    string inf;
+    while (getline(cin, inf)) {
+        cout << inf << "\n";
+    }
+}
+
 signed main() {
     cout << "Your name: ";
     string name; cin >> name;
@@ -10,6 +18,6 @@ signed main() {
     cout << "Book you recommend reading: ";
     string emp; getline(cin, emp);
     string book; getline(cin, book);
-    ofstream f; f.open("C:\\Users\\Alexander\\Desktop\\survey\\results.txt", ios::app);
+    ofstream f; f.open("results.txt", ios::app);
     f << name << ' ' << surname << ' ' << book << "\n";
 }
