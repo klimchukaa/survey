68382b052f440cf570bbbc43abac22047fb06fdf
Aleksandr <klimchuk.aa@students.sch2.ru>
2021-09-15 10:17:18 +0300

    Added choice between survey and results

--- main.cpp
+++ main.cpp
@@ -5,12 +5,18 @@ using namespace std;
 void get_results() {
     ifstream in; in.open("results.txt");
     string inf;
-    while (getline(cin, inf)) {
+    while (getline(in, inf)) {
         cout << inf << "\n";
     }
 }
 
 signed main() {
+    cout << "What do you need? ";
+    string need; getline(cin, need);
+    if (need == "results") {
+        get_results();
+        return 0;
+    }
     cout << "Your name: ";
     string name; cin >> name;
     cout << "Your surname: ";
